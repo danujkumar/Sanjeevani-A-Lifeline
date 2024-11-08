9fc2715687533caf01428363e92d24111833a9a5
Anuj Singh <anuj.as828@gmail.com>
2024-11-08 11:35:28 +0530

    restructuring

--- src/store/auth.jsx
+++ src/store/auth.jsx
@@ -6,6 +6,7 @@ import Login from "../components/login/Login";
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import { action } from "../store";
+import { io } from 'socket.io-client'
 
 export const AuthProvider = ({ children }) => {
 
@@ -15,8 +16,24 @@ export const AuthProvider = ({ children }) => {
     return localStorage.setItem("token", serverToken);
   };
 
+  const socket = io("http://localhost:3000")
+
+  const twoWay = () => {
+    console.log("Two way communication...")
+    
+  }
+  
+
+
+
   useEffect(()=>{
     console.log("fetching the profile from redux")
+    // twoWay();
+    socket.on("connect", ()=>{
+      console.log("connected", socket.id)
+    })
+
+    socket.on("Testing", (res)=>{console.log(res)})
     dispatch(action.getDocInfo())
     dispatch(action.getUserInfo());
   }, [])
